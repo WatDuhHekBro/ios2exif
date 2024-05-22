5801049388c2b49844447b976e26c2dd5cf3086e
WatDuhHekBro <watduhhekbro@protonmail.com>
2024-05-22 01:40:14 -0500

    feat: Added support for QuickTime metadata

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,17 @@ use std::{
     env,
     fs::{self, File},
     io::BufReader,
-    path::Path,
+    path::{Path, PathBuf},
+    process::Command,
+    str,
 };
+
+// "CreationDate" is actually QuickTime metadata, not EXIF metadata. (https://superuser.com/a/1285932)
+// "-s3" is to just get the value without the header. (https://photo.stackexchange.com/a/56678)
+// Example: "exiftool -DateTimeOriginal -s3 2023-05-25_19-47-30.heic" gives value "2023:05:25 19:47:30"
+// Example: "exiftool -CreationDate -s3 2023-05-14_21-34-06.mov" gives value "2023:05:14 21:34:06-05:00"
+// Example: "exiftool -CreateDate -s3 2023-05-14_21-34-06.mp4" gives value "2023:05:14 21:34:06" (timezone-unaware, manual checking required)
+// "for file in ./*; do echo $file; exiftool -CreateDate -s3 $file | tr ' ' _ | tr : -; echo; done" (plus manual checking for timezone)
 
 // Maybe implement command line arguments that target specific files.
 fn main() {
@@ -14,8 +23,11 @@ fn main() {
         env!("CARGO_PKG_VERSION")
     );
 
+    // Get file iterator of current directory
     let Ok(current_directory) = env::current_dir() else {
-        return eprintln!("The current working directory either doesn't exist or isn't accessible.");
+        return eprintln!(
+            "The current working directory either doesn't exist or isn't accessible."
+        );
     };
     let Ok(files) = fs::read_dir(current_directory) else {
         return eprintln!("The current working directory isn't a valid directory.");
@@ -33,49 +45,56 @@ fn main() {
         };
         let path = file.path();
         let path_str = path.to_string_lossy().to_string();
+        // Get lowercase extension (if any)
+        let extension = {
+            let extension = path.extension();
+
+            match extension {
+                Some(extension) => Some(extension.to_string_lossy().to_lowercase()),
+                None => None,
+            }
+        };
 
         // Ignore directories
-        if !path.is_dir() {
-            let Ok(file) = File::open(&path) else {
-                eprintln!("Warning: Failed to open the file: \"{}\"", path_str);
-                needs_confirmation = true;
-                continue;
-            };
-            let mut bufreader = BufReader::new(&file);
-            let exifreader = exif::Reader::new();
+        if path.is_dir() {
+            continue;
+        }
 
-            // Ignore the file if invalid exif
-            let Ok(exif) = exifreader.read_from_container(&mut bufreader) else {
-                eprintln!("Warning: The file doesn't contain valid EXIF: \"{}\"", path_str);
-                needs_confirmation = true;
-                continue;
-            };
+        // Determine what to do based on the file extension
+        let try_exif_first = match extension {
+            Some(extension) => match extension.as_str() {
+                // Photos
+                "jpg" => true,
+                "jpeg" => true,
+                "png" => true,
+                "heic" => true,
+                // Videos
+                "mov" => false,
+                "mp4" => false,
+                _ => {
+                    println!("Warning: Unsupported extension \".{extension}\", ignoring...");
+                    continue;
+                }
+            },
+            _ => true,
+        };
 
-            // Extract the EXIF field DateTimeOriginal, use it as the new filename.
-            // Do not use DateTime, as that's supposed to update if the image is modified (https://gitlab.gnome.org/GNOME/gimp/-/issues/8160).
-            // Ignore if "ifd_num" isn't "primary", as that indicates that it's a thumbnail image, not a main image.
-            let Some(datetime) = exif.get_field(exif::Tag::DateTimeOriginal, exif::In::PRIMARY) else {
-                eprintln!("Warning: EXIF metadata is present but does not include DateTimeOriginal for file \"{path_str}\". Not renaming...");
-                needs_confirmation = true;
-                continue;
-            };
+        let timestamp = get_timestamp(&path, &path_str, try_exif_first);
+        let Some(timestamp) = timestamp else {
+            needs_confirmation = true;
+            continue;
+        };
 
-            // You don't actually need to use regex for this, just replace spaces and colons.
-            let mut timestamp = datetime.display_value().to_string(); // Format: "YYYY-MM-DD HH:MM:SS"
-            timestamp = timestamp.replace(" ", "_");
-            timestamp = timestamp.replace(":", "-");
-
-            // Error if two files have the same timestamp, as that will definitely cause problems.
-            // Continue the loop to show all occurrences.
-            if map.contains_key(&timestamp) {
-                eprintln!(
+        // Error if two files have the same timestamp, as that will definitely cause problems.
+        // Continue the loop to show all occurrences.
+        if map.contains_key(&timestamp) {
+            eprintln!(
                     "Error: Attempted to add \"{path_str}\"\n\t...but the timestamp ({timestamp}) already exists in file: \"{}\"",
                     map[&timestamp]
                 );
-                must_exit = true;
-            } else {
-                map.insert(timestamp, path_str);
-            }
+            must_exit = true;
+        } else {
+            map.insert(timestamp, path_str);
         }
     }
 
@@ -127,4 +146,101 @@ fn main() {
             println!("Renaming success for \"{path}\" to timestamp \"{timestamp}\".");
         }
     }
+}
+
+// Try exif first, otherwise use "exiftool" to read miscellaneous metadata (QuickTime, etc.)
+fn get_timestamp(path: &PathBuf, path_str: &String, try_exif_first: bool) -> Option<String> {
+    if try_exif_first {
+        let timestamp = get_timestamp_from_exif(&path, &path_str);
+
+        match timestamp {
+            Ok(timestamp) => return Some(timestamp),
+            Err(error_message) => {
+                eprintln!("{}", error_message);
+            }
+        }
+    }
+
+    get_timestamp_from_exiftool(path_str)
+}
+
+// Returns a string of the formatted timestamp if successful
+// Ignore the entry if unsuccessful
+fn get_timestamp_from_exif(path: &PathBuf, path_str: &String) -> Result<String, String> {
+    let Ok(file) = File::open(path) else {
+        return Err(format!(
+            "Warning: Failed to open the file: \"{}\"",
+            path_str
+        ));
+    };
+    let mut bufreader = BufReader::new(&file);
+    let exifreader = exif::Reader::new();
+
+    // Ignore the file if invalid exif
+    let Ok(exif) = exifreader.read_from_container(&mut bufreader) else {
+        return Err(format!(
+            "Warning: The file doesn't contain valid EXIF: \"{}\"",
+            path_str
+        ));
+    };
+
+    // Extract the EXIF field DateTimeOriginal, use it as the new filename.
+    // Do not use DateTime, as that's supposed to update if the image is modified (https://gitlab.gnome.org/GNOME/gimp/-/issues/8160).
+    // Ignore if "ifd_num" isn't "primary", as that indicates that it's a thumbnail image, not a main image.
+    let Some(datetime) = exif.get_field(exif::Tag::DateTimeOriginal, exif::In::PRIMARY) else {
+        return Err(format!("Warning: EXIF metadata is present but does not include DateTimeOriginal for file \"{path_str}\". Not renaming..."));
+    };
+
+    // Convert timestamp of format "YYYY-MM-DD HH:MM:SS" to "YYYY-MM-DD_HH-MM-SS"
+    let mut timestamp = datetime.display_value().to_string();
+    // You don't actually need to use regex for this, just replace spaces and colons.
+    timestamp = timestamp.replace(" ", "_");
+    timestamp = timestamp.replace(":", "-");
+
+    Ok(timestamp)
+}
+
+// If it fails for whatever reason, just ignore the entry
+fn get_timestamp_from_exiftool(path_str: &String) -> Option<String> {
+    // Format: "YYYY:MM:DD HH:MM:SS-ZZ:00"
+    let output = Command::new("exiftool")
+        .arg("-CreationDate")
+        .arg("-s3")
+        .arg(path_str)
+        .output();
+
+    let Ok(output) = output else {
+        eprintln!(
+            "[exiftool] Warning: Failed to execute \"exiftool\" process on path \"{path_str}\"!"
+        );
+        return None;
+    };
+
+    let length = output.stdout.len();
+
+    // Output is empty if metadata attribute doesn't exist
+    if length <= 0 {
+        eprintln!("[exiftool] Warning: No output for tag \"CreationDate\" on path \"{path_str}\"!");
+        return None;
+    }
+
+    // -1 for ending newline
+    // -6 for timezone
+    let slice = &output.stdout[0..length - 7];
+    let timestamp = str::from_utf8(slice);
+
+    let Ok(timestamp) = timestamp else {
+        eprintln!(
+            "[exiftool] Warning: CreationDate \"{:?}\" should be a valid UTF-8 string on path \"{path_str}\"!",
+            slice
+        );
+        return None;
+    };
+
+    let mut timestamp = timestamp.to_string();
+    // Convert timestamp of format "YYYY:MM:DD HH:MM:SS" to "YYYY-MM-DD_HH-MM-SS"
+    timestamp = timestamp.replace(" ", "_");
+    timestamp = timestamp.replace(":", "-");
+
+    Some(timestamp)
 }
